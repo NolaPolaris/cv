deee59ef1ce4c8b74cbabbb4a7e412e17ec4c734
nolapolaris <julietaubry@gmail.com>
2022-02-02 20:27:41 +0100

    add svg-loader + fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-    
+      fontFamily: {
+        title: ['Rubik Mono One', 'sans-serif'],
+        body: ['Roboto Mono', 'monospace']
+      },
+      
+      boxShadow: {
+        default: '10px 10px 0 rgba(0, 0, 0, 0.3)',
+      },
+
+      colors:{
+          yellow:{
+            default:'',
+            50:'#fbff0078'
+          },
+          blue:{},
+          pink:{},
+          green:{},
+      }
     },
   },
   variants: {

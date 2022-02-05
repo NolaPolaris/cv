e23addf1ddffcef436ad1b2c3d62db7e89c2bb2c
maxime <maxime.peresson@actio.com>
2022-02-05 13:52:04 +0100

    webpack assets config

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,74 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-module.exports = {
-  mode: 'production',
-  entry: './src/js/index.js',
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const prod = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: {
+    cv: path.resolve(__dirname, 'src') + '/js/index.js',
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[name].[chunkhash:3].js',
+    path: path.resolve('dist')
   },
   module: {
     rules: [
       {
-        test: /\.js$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        test: /\.js$/,
+        loader: 'esbuild-loader'
       },
       {
-        test: /\.(sass|less|css)$/,
-        include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }        
       },
       {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      },
-      // {
-      //   test: /\.(png|svg|jpe?g|gif)$/,
-      //   include: path.resolve(__dirname, 'src'),
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         name: '[name].[ext]',
-      //         outputPath: 'images/',
-      //         publicPath: 'images/'
-      //       }
-      //     }
-      //   ]
-      // },      
+        test: /\.(svg)$/i,
+        type: 'asset/inline',   
+      },      
       {
-        test: /\.(png|jpg|gif)$/i,
-        type: 'asset/resource'
+        test: /\.(woff2|ttf|eot)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
+        }        
+      },      
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { url: true } },
+          "postcss-loader",
+        ],
       },
-      
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },      
     ],
   },
+  optimization: {
+    minimize: prod,
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        css: true
+      })
+    ]
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
-    })
-  ],  
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    watchContentBase: true,
-  },
+    }),
+  ],
+  mode: prod ? 'production' : 'development',
+  stats: prod ? 'normal' : 'minimal'
 };
+
+module.exports = config;

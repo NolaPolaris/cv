c364850b0ffafbfc27101183d9e5dbbfa194db50
nolapolaris <julietaubry@gmail.com>
2022-02-05 17:52:06 +0100

    style header + onglet

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 import '../css/style.css';
 import './api.js'
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 2800, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       },
       
       boxShadow: {
-        default: '10px 10px 0 rgba(0, 0, 0, 0.3)',
+        shadow: '5px 5px 0 rgba(0, 0, 0, 1)',
       },
 
       colors:{

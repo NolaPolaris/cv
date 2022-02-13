b0a4df200669283de6b491ff86f9acae65aa3973
Julie <julietaubry@gmail.com>
2022-02-13 13:30:35 +0100

    affichage dynamiques des onglets

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,30 @@ import './api.js'
 import * as $ from 'jquery';
 
 $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 500, function(){
-          window.location.hash = hash;
-        });
-      } 
-    });
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 100, function(){
+        window.location.hash = hash;
+      });
+    } 
   });
+  
+  
+  $('.onglet').on('click', function (e) {
+    e.preventDefault();
+    $('.onglet').removeClass('active');
+    $(this).addClass('active');
+    let title = $(this).data('title');
+    let current = $('.onglet-content[data-content="' + title + '"]');
+    current.attr('id', title);
+    $('.onglet-content').removeClass('active');
+    current.removeClass('hidden');
+    current.addClass('active');
+  })
+  
+});

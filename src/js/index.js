549b35894311ee8518327bb7c2c72ed2d4ac6f9a
Julie <julietaubry@gmail.com>
2022-02-16 15:56:15 +0100

    xp mobile ok

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   $("a").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
-      var hash = this.hash;
+      let hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 100, function(){
@@ -15,21 +15,38 @@ $(document).ready(function(){
       });
     } 
   });
+
+
   
-  if (window.innerWidth >= 769) {
-    $('.onglet').on('click', function (e) {
-      e.preventDefault();
-      $('.onglet').removeClass('active');
-      $(this).addClass('active');
-      let title = $(this).data('title');
-      let current = $('.onglet-content[data-content="' + title + '"]');
-      current.attr('id', title);
-      $('.onglet-content').removeClass('active');
-      current.removeClass('hidden');
-      current.addClass('active');
+  $('.onglet').on('click', function () {
+    let siblings = $(this).siblings('.onglet');
+    siblings.removeClass('active');
+  
+      if (window.innerWidth >= 769) {
+        $(this).addClass('active');
+        let title = $(this).data('title');
+        let current = $('.onglet-content[data-content="' + title + '"]');
+        current.attr('id', title);
+        $('.onglet').removeClass('active');
+        $('.onglet-content').removeClass('active');
+        $(this).addClass('active');
+        current.removeClass('hidden');
+        current.addClass('active');
+  
+      } else {
+        let current = $(this).children('.onglet-content-xs'); 
+        let parent = $(this);
+
+        if(parent.hasClass('active')){
+          current.slideUp(300, function(){
+            parent.removeClass('active')
+          }); 
+        }else{ 
+          parent.addClass('active');
+          current.slideDown(300);
+        }
+        
+      }
     })
     
-  }
-  
- 
 });
